6e600504d95307716a0d173dc9676c546157a797
miloujb <emilyjbailey@hotmail.co.uk>
2019-09-24 20:18:14 +0100

    ball added

--- pong.py
+++ pong.py
@@ -29,6 +29,13 @@ paddle_b.goto(350, 0)
 
 # Ball
 
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
+
 #Main Game Loop
 while True:
     wn.update()
