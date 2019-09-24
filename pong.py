b1027486b9d7427b013ec85f94544f5b577fdc02
miloujb <emilyjbailey@hotmail.co.uk>
2019-09-24 20:23:53 +0100

    paddle a moves up and down

--- pong.py
+++ pong.py
@@ -36,6 +36,24 @@ ball.color("white")
 ball.penup()
 ball.goto(0, 0)
 
+#Function
+
+def paddle_a_up():
+    y = paddle_a.ycor()
+    y +=20
+    paddle_a.sety(y)
+
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y)
+
+# Keyboard binding
+
+wn.listen()
+wn.onkeypress(paddle_a_up, "w")
+wn.onkeypress(paddle_a_down, "s")
+
 #Main Game Loop
 while True:
     wn.update()
